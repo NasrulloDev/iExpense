92d7fc960cacc1cdb87df5a83423fb12a34b98af
unknown7761 <ismoilzanovnasrullo@gmail.com>
2024-02-01 15:32:10 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Насрулло Исмоилжонов on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
